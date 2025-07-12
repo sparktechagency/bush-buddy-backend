fc68fe89732792316446bc2da6121e9ae154f422
webashraf <aliashraftamim@gmail.com>
2025-07-12 12:58:38 +0600

    make link filed only for youtube video

--- src/app/modules/contextual/tipsAndTricks/tips.validation.ts
+++ src/app/modules/contextual/tipsAndTricks/tips.validation.ts
@@ -4,7 +4,19 @@ const createTips = z.object({
   body: z
     .object({
       title: z.string().optional(),
-      link: z.string().url({ message: "Link must be a valid URL" }),
+      link: z
+        .string()
+        .url({ message: "Link must be a valid URL" })
+        .refine(
+          (val) =>
+            /^https?:\/\/(www\.)?(youtube\.com\/watch\?v=|youtu\.be\/)[\w-]{11}/.test(
+              val
+            ),
+          {
+            message: "Link must be a valid YouTube video URL",
+          }
+        ),
+      // link: z.string().url({ message: "Link must be a valid URL" }),
       platform: z
         .enum(["youtube", "google", "linkedin", "facebook"])
         .optional(),
